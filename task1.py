7b0030ca68dca5863de92d8eb6c1f80b8e6ff991
shreyapgosai <me240003031@iiti.ac.in>
2025-02-10 19:43:48 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -144,3 +144,33 @@ def hybrid_recommend(user_id, movie):
 
 recommend('Avatar')
 print(hybrid_recommend(user_id=6, movie='Avatar'))
+import streamlit as st
+import pickle
+import pandas as pd
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        movie_id=i[0]
+
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+    return recommended_movies
+
+movies_dict=pickle.load(open('movie_dict.pkl','rb'))
+movies=pd.DataFrame(movies_dict)
+similarity=pickle.load(open('similarity.pkl','rb'))
+
+st.title('movie recommendation System')
+option = st.selectbox(
+    "How would you like to be contacted?",
+    movies['title'].values
+)
+if st.button('recommend'):
+    recommendations=recommend(option)
+    for i in recommendations:
+        st.write(i)
